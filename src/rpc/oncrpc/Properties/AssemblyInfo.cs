a7d08e2fbdb6067e7d2d392233b2c4300964de3c
David Hary <791370+ATECoder@users.noreply.github.com>
2025-09-18 12:55:49 -0700

    - Tests pending. - Use language preview in windows and net standard projects so as to enable auto properties. - Implement code analysis recommendations. - Unify project file structure. - Remove legacy leftover Visual Basic project file entries. - Remove unused core lib and net standard code. - Move assembly information such as CLS Compliance and COM visibility to the project file. - Remove words that are no longer tagged as misspelled from the exclusion dictionary file. - Remove references to Std.Net21 project. - Remove the old exclusion dictionary (exclusion.dix and dix.xml) file. - Use Internal accessibility in .net standard 2.1 classes. - MS Test: Add cancellation token to the wait task. - Update test SDK from 3.10.3 to 3.10.4.

--- src/rpc/oncrpc/Properties/AssemblyInfo.cs
+++ src/rpc/oncrpc/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 
-[assembly: System.CLSCompliant( false )]
-[assembly: System.Runtime.InteropServices.ComVisible( false )]
+// in source file:
+// [assembly: System.CLSCompliant( false )]
+// [assembly: System.Runtime.InteropServices.ComVisible( false )]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "cc.isr.ONC.RPC.MSTest,PublicKey=" + cc.isr.ONC.RPC.SolutionInfo.PublicKey )]
