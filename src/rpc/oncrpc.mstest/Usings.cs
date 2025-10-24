4ac515386d1249a325c765d0bbee01d14e37a76f
David Hary <791370+ATECoder@users.noreply.github.com>
2025-10-24 16:27:45 -0700

    - Remove Std MSTest project from solution. - Add Exists to settings file and remove enabled and all from settigns files and classes. - MS Test:   - Remove reference to the cc.isr.Std.MSTest project.   - Replace test site settings with Location Settings subclass of the JSon location settings base.   - Use Settings base class from the JSon Application Settings library.   - Use Settings Container base class from the JSon Application Settings library.

--- src/rpc/oncrpc.mstest/Usings.cs
+++ src/rpc/oncrpc.mstest/Usings.cs
@@ -2,7 +2,9 @@ global using Microsoft.Extensions.Logging;
 global using System.Net;
 global using cc.isr.XDR;
 global using cc.isr.XDR.Codecs;
-global using cc.isr.Std.Tests;
+global using using cc.isr.Std.Logging;
+using cc.isr.Std.Logging.ILoggerExtensions;
+using cc.isr.Std.RandomExtensions;
 using cc.isr.Std.Tests.Extensions;
 
 
