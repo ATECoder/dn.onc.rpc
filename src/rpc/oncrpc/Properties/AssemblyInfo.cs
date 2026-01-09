93046f46abe441c909398fee5abe3c199b5806db
David Hary <791370+ATECoder@users.noreply.github.com>
2026-01-09 10:12:35 -0800

    - Simplify null check. - Remove empty top lines.

--- src/rpc/oncrpc/Properties/AssemblyInfo.cs
+++ src/rpc/oncrpc/Properties/AssemblyInfo.cs
@@ -1,5 +1,2 @@
 
-// in source file:
-// [assembly: System.CLSCompliant( false )]
-// [assembly: System.Runtime.InteropServices.ComVisible( false )]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "cc.isr.ONC.RPC.MSTest,PublicKey=" + cc.isr.ONC.RPC.SolutionInfo.PublicKey )]
